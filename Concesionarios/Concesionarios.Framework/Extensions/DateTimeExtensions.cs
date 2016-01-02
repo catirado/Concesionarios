c10f8178250b2de6eeb6f9a58719b9873a9f1308
Carlos Tirado <catirado87@gmail.com>
2016-01-02 23:59:54 +0100

    Added some extensions and utility classes

---
+++ Concesionarios/Concesionarios.Framework/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Concesionarios.Framework.Extensions
+{
+    /// <summary>
+    /// DateTime Extensions
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        #region Elapsed extension
+        /// <summary>
+        /// Elapseds the time.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns>TimeSpan</returns>
+        public static TimeSpan Elapsed(this DateTime datetime)
+        {
+            return DateTime.Now - datetime;
+        }
+        #endregion
+
+        #region Week of year
+        /// <summary>
+        /// Weeks the of year.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="weekrule">The weekrule.</param>
+        /// <param name="firstDayOfWeek">The first day of week.</param>
+        /// <returns></returns>
+        public static int WeekOfYear(this DateTime datetime, System.Globalization.CalendarWeekRule weekrule, DayOfWeek firstDayOfWeek)
+        {
+            System.Globalization.CultureInfo ciCurr = System.Globalization.CultureInfo.CurrentCulture;
+            return ciCurr.Calendar.GetWeekOfYear(datetime, weekrule, firstDayOfWeek);
+        }
+        /// <summary>
+        /// Weeks the of year.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="firstDayOfWeek">The first day of week.</param>
+        /// <returns></returns>
+        public static int WeekOfYear(this DateTime datetime, DayOfWeek firstDayOfWeek)
+        {
+            System.Globalization.DateTimeFormatInfo dateinf = new System.Globalization.DateTimeFormatInfo();
+            System.Globalization.CalendarWeekRule weekrule = dateinf.CalendarWeekRule;
+            return WeekOfYear(datetime, weekrule, firstDayOfWeek);
+        }
+        /// <summary>
+        /// Weeks the of year.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="weekrule">The weekrule.</param>
+        /// <returns></returns>
+        public static int WeekOfYear(this DateTime datetime, System.Globalization.CalendarWeekRule weekrule)
+        {
+            System.Globalization.DateTimeFormatInfo dateinf = new System.Globalization.DateTimeFormatInfo();
+            DayOfWeek firstDayOfWeek = dateinf.FirstDayOfWeek;
+            return WeekOfYear(datetime, weekrule, firstDayOfWeek);
+        }
+        /// <summary>
+        /// Weeks the of year.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="weekrule">The weekrule.</param>
+        /// <returns></returns>
+        public static int WeekOfYear(this DateTime datetime)
+        {
+            System.Globalization.DateTimeFormatInfo dateinf = new System.Globalization.DateTimeFormatInfo();
+            System.Globalization.CalendarWeekRule weekrule = dateinf.CalendarWeekRule;
+            DayOfWeek firstDayOfWeek = dateinf.FirstDayOfWeek;
+            return WeekOfYear(datetime, weekrule, firstDayOfWeek);
+        }
+        #endregion
+
+        #region Get Datetime for Day of Week
+        /// <summary>
+        /// Gets the date time for day of week.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="day">The day.</param>
+        /// <param name="firstDayOfWeek">The first day of week.</param>
+        /// <returns></returns>
+        public static DateTime GetDateTimeForDayOfWeek(this DateTime datetime, DayOfWeek day, DayOfWeek firstDayOfWeek)
+        {
+            int current = DaysFromFirstDayOfWeek(datetime.DayOfWeek, firstDayOfWeek);
+            int resultday = DaysFromFirstDayOfWeek(day, firstDayOfWeek);
+            return datetime.AddDays(resultday - current);
+        }
+        public static DateTime GetDateTimeForDayOfWeek(this DateTime datetime, DayOfWeek day)
+        {
+            System.Globalization.DateTimeFormatInfo dateinf = new System.Globalization.DateTimeFormatInfo();
+            DayOfWeek firstDayOfWeek = dateinf.FirstDayOfWeek;
+            return GetDateTimeForDayOfWeek(datetime, day, firstDayOfWeek);
+        }
+        /// <summary>
+        /// Firsts the date time of week.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns></returns>
+        public static DateTime FirstDateTimeOfWeek(this DateTime datetime)
+        {
+            System.Globalization.DateTimeFormatInfo dateinf = new System.Globalization.DateTimeFormatInfo();
+            DayOfWeek firstDayOfWeek = dateinf.FirstDayOfWeek;
+            return FirstDateTimeOfWeek(datetime, firstDayOfWeek);
+        }
+        /// <summary>
+        /// Firsts the date time of week.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <param name="firstDayOfWeek">The first day of week.</param>
+        /// <returns></returns>
+        public static DateTime FirstDateTimeOfWeek(this DateTime datetime, DayOfWeek firstDayOfWeek)
+        {
+            return datetime.AddDays(-DaysFromFirstDayOfWeek(datetime.DayOfWeek, firstDayOfWeek));
+        }
+
+        /// <summary>
+        /// Dayses from first day of week.
+        /// </summary>
+        /// <param name="current">The current.</param>
+        /// <param name="firstDayOfWeek">The first day of week.</param>
+        /// <returns></returns>
+        private static int DaysFromFirstDayOfWeek(DayOfWeek current, DayOfWeek firstDayOfWeek)
+        {
+            //Sunday = 0,Monday = 1,...,Saturday = 6
+            int daysbetween = current - firstDayOfWeek;
+            if (daysbetween < 0) daysbetween = 7 + daysbetween;
+            return daysbetween;
+        }
+        #endregion
+
+        public static string GetValueOrDefaultToString(this DateTime? datetime, string defaultvalue)
+        {
+            if (datetime == null) return defaultvalue;
+            return datetime.Value.ToString();
+        }
+
+        public static string GetValueOrDefaultToString(this DateTime? datetime, string format, string defaultvalue)
+        {
+            if (datetime == null) return defaultvalue;
+            return datetime.Value.ToString(format);
+        }
+
+        public static string MonthYearFormat(this DateTime? datetime)
+        {
+            if (datetime.HasValue)
+            {
+                System.Globalization.CultureInfo ciCurr = System.Globalization.CultureInfo.CurrentCulture;
+
+
+                string month = ciCurr.DateTimeFormat.GetAbbreviatedMonthName(datetime.Value.Month).ToUpperInvariant();
+                string year = datetime.Value.Year.ToString().Substring(2);
+                return month + "." + year;
+            }
+            else
+                return String.Empty;
+
+        }
+
+        public static string BeautifyPeriod(this DateTime fromDate, DateTime toDate, bool withYear = true)
+        {
+            var cultureInfo = CultureInfo.CurrentCulture;
+            var textInfo = cultureInfo.TextInfo;
+            var dateTimeFormat = cultureInfo.DateTimeFormat;
+
+            if (fromDate.Date == toDate.Date)
+                return withYear ? fromDate.ToLongDateString() : fromDate.ToShortDateString();
+            else if (fromDate.FirstDateOfMonth() == fromDate && toDate.LastDateOfMonth() == toDate)
+            {
+                if (fromDate.Year == toDate.Year)
+                {
+                    var fromMonthName = textInfo.ToTitleCase(dateTimeFormat.GetMonthName(fromDate.Month));
+                    var toMonthName = textInfo.ToTitleCase(dateTimeFormat.GetMonthName(toDate.Month));
+
+                    if (fromDate.Month == toDate.Month)
+                        return withYear ? fromMonthName + " " + fromDate.Year.ToString() : fromMonthName;
+                    else
+                        return withYear ? fromMonthName + " - " + toMonthName + " " + fromDate.Year.ToString()
+                            : fromMonthName + " - " + toMonthName;
+                }
+                else
+                {
+                    return textInfo.ToTitleCase(fromDate.ToString(dateTimeFormat.YearMonthPattern))
+                            + " - " +
+                            textInfo.ToTitleCase(toDate.ToString(dateTimeFormat.YearMonthPattern));
+                }
+            }
+
+            return fromDate.ToShortDateString() + " - " + toDate.ToShortDateString();
+        }
+
+        public static DateTime LastDateOfMonth(this DateTime date)
+        {
+            var dtTo = date;
+            dtTo = dtTo.AddMonths(1);
+            dtTo = dtTo.AddDays(-(dtTo.Day));
+            return dtTo;
+        }
+
+        public static DateTime FirstDateOfMonth(this DateTime date)
+        {
+            var dtFrom = date;
+            dtFrom = dtFrom.AddDays(-(dtFrom.Day - 1));
+            return dtFrom;
+        }
+    }
+}
