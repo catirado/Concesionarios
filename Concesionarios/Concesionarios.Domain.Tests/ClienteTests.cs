825997fa1225bd763323de50516b401821661e1d
Carlos Tirado <catirado87@gmail.com>
2016-01-03 15:36:40 +0100

    added api project to solution

--- Concesionarios/Concesionarios.Domain.Tests/ClienteTests.cs
+++ Concesionarios/Concesionarios.Domain.Tests/ClienteTests.cs
@@ -42,5 +42,46 @@ namespace Concesionarios.Domain.Tests
         }
 
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotChangeNombreWithEmptyNombre()
+        {
+            var cliente = new Cliente(0, "nombre", "apellido", "telefono", true);
+            cliente.ChangeNombre(String.Empty, "apellido");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotChangeNombreWithNullNombre()
+        {
+            var cliente = new Cliente(0, "nombre", "apellido", "telefono", true);
+            cliente.ChangeNombre(null, "apellido");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotChangeNombreWithEmptyApellidos()
+        {
+            var cliente = new Cliente(0, "nombre", "apellido", "telefono", true);
+            cliente.ChangeNombre("nombre", String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotChangeNombreWithNullApellidos()
+        {
+            var cliente = new Cliente(0, "nombre", "apellido", "telefono", true);
+            cliente.ChangeNombre("nombre", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotChangeTelefonoWithNullTelefono()
+        {
+            var cliente = new Cliente(0, "nombre", "apellido", "telefono", true);
+            cliente.ChangeTelefono(null);
+        }
+
     }
 }
