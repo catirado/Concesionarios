9a6d58f9ba3690459de9929015537d566e849897
Carlos Tirado <catirado87@gmail.com>
2016-02-06 12:17:52 +0100

    remove unused classes

--- Concesionarios/Concesionarios.UI.Web/App_Start/Startup.Auth.cs
+++ Concesionarios/Concesionarios.UI.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using Concesionarios.UI.Web.Models;
 
 namespace Concesionarios.UI.Web
 {
