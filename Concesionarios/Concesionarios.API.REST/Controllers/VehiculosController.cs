78d2750390f8d14daa97e1dc216d957ff73067a6
Carlos Tirado <catirado87@gmail.com>
2016-02-06 11:27:23 +0100

    addesd controller and remove unused refrences

---
+++ Concesionarios/Concesionarios.API.REST/Controllers/VehiculosController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Concesionarios.API.REST.Controllers
+{
+    //http://chsakell.com/2015/05/10/asp-net-web-api-unit-testing/
+    public class VehiculosController : ApiController
+    {
+
+    }
+}
